93c43f4c57676b1fdbe4e01e7342004709bdae33
chaowlert <chaowlert@gmail.com>
2016-01-02 03:02:51 +0700

    upgrade version

--- AzurePush/AzurePush/Extensions.cs
+++ AzurePush/AzurePush/Extensions.cs
@@ -1,7 +1,4 @@
-﻿using AzureStorageExtensions;
-using Microsoft.WindowsAzure.Storage.Table;
-
-namespace AzurePush
+﻿namespace AzurePush
 {
     internal static class Extensions
     {
@@ -9,24 +6,6 @@ namespace AzurePush
         {
             var i = userId.GetHashCode() % 100000;
             return i.ToString("00000") + "|" + userId;
-        }
-
-        public static void SafeDelete<T>(this CloudTable<T> table, string partitionKey, string rowKey) where T : class, ITableEntity, new()
-        {
-            try
-            {
-                table.Delete(partitionKey, rowKey);
-            }
-            catch { }
-        }
-
-        public static void SafeDelete<T>(this CloudTable<T> table, T entity) where T : class, ITableEntity, new()
-        {
-            try
-            {
-                table.Delete(entity);
-            }
-            catch { }
         }
     }
 }
