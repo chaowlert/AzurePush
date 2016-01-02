a55054c55bdec5808c006baa76f77ec0e5f00e2c
chaowlert <chaowlert@gmail.com>
2016-01-02 17:26:20 +0700

    remove framework extension reference

--- AzurePush/AzurePush/Extensions.cs
+++ AzurePush/AzurePush/Extensions.cs
@@ -1,7 +1,32 @@
-﻿namespace AzurePush
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AzurePush
 {
     internal static class Extensions
     {
+        public static string Utf8Substring(this string text, int byteLimit)
+        {
+            int byteCount = 0;
+            char[] buffer = new char[1];
+            for (int i = 0; i < text.Length; i++)
+            {
+                buffer[0] = text[i];
+                byteCount += Encoding.UTF8.GetByteCount(buffer);
+                if (byteCount > byteLimit)
+                {
+                    // Couldn't add this character. Return its index
+                    return text.Substring(0, i);
+                }
+            }
+            return text;
+        }
+
+        public static string RemoveSign(this string text)
+        {
+            return Regex.Replace(text, @"\W+", "");
+        }
+
         public static string PrependHash(this string userId)
         {
             var i = userId.GetHashCode() % 100000;
